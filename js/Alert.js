5d2d2c66c0df50f7aabda4669f9efe5d93999027
HabibAroua <habibha.aroua82@gmail.com>
2018-08-26 01:20:44 +0100

    using

---
+++ js/Alert.js
@@ -0,0 +1,15 @@
+function validation()
+{
+    swal(
+        'The Internet?',
+        'That thing is still around?',
+        'question'
+        );
+}
+         
+function basicMessage(text)
+{
+    swal(text);
+}
+
+//https://sweetalert2.github.io/
