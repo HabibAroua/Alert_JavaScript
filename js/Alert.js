7a1d542dba45547d664cfb1b0d1839bd02a14fb9
HabibAroua <habibha.aroua82@gmail.com>
2018-09-02 15:29:12 +0100

    add message error

--- js/Alert.js
+++ js/Alert.js
@@ -1,15 +1,29 @@
 function validation()
 {
-    swal(
+    swal
+    (
         'The Internet?',
         'That thing is still around?',
         'question'
-        );
+    );
 }
          
 function basicMessage(text)
 {
     swal(text);
+}
+
+function errorMessage(title,text,footer)
+{
+    swal
+    (
+        {
+            type: 'error',
+            title: title,
+            text: text,
+            footer: footer
+        }
+    );
 }
 
 //https://sweetalert2.github.io/
